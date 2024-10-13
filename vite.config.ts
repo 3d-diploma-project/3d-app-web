7a2e49047e5008ebc853c06d45cdb00889498052
ut5tw <sheredan2003@gmail.com>
2024-10-13 23:15:56 +0300

    Setup testing with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 import restart from 'vite-plugin-restart'
+import { configDefaults } from 'vitest/config'
+
+const exclude = [
+  ...configDefaults.exclude,
+  '**/translations/**',
+  '*config.js',
+  '*config.ts',
+  './vite-env.d.ts',
+  './src/main.tsx',
+  './src/App.tsx',
+  './src/vite-env.d.ts'
+]
 
 export default defineConfig({
-  plugins: [react(), glsl(), restart({ restart: ['../public/**'] })]
+  plugins: [react(), glsl(), restart({ restart: ['../public/**'] })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    exclude,
+    coverage: {
+      reportsDirectory: './src/tests/coverage',
+      exclude
+    }
+  }
 })
