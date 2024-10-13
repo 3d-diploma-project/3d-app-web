393756f883bdd65bfeb35c3f31215dab6e43769b
ut5tw <sheredan2003@gmail.com>
2024-10-13 16:30:58 +0300

    Add husky with lint-staged

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -4,11 +4,12 @@ import cn from '../utils/cn'
 interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
   className?: string
+  prop: string
 }
 
 const AppButton = ({ children, className = '', ...props }: AppButtonProps) => {
   return (
-    <button {...props} className={cn('hover: rounded-md bg-[#1E0094] hover:bg-[#1e0094af] px-2 py-1', className)}>
+    <button {...props} className={cn('hover: rounded-md bg-[#1E0094] px-2 py-1 hover:bg-[#1e0094af]', className)}>
       {children}
     </button>
   )
