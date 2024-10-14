574db31c2d4bdf53973c09ee66cd59bd5ff45ebd
ut5tw <sheredan2003@gmail.com>
2024-10-14 17:13:03 +0300

    Fix sonar issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { useTranslation } from 'react-i18next'
-
-import Experience from './Experience'
-import DragAndDrop from './components/DragAndDrop'
 import { LanguageSelector } from './components/LanguageSelector'
 
 const App = () => {
   const { t } = useTranslation()
-  async function callback(files: File[]) {
-    const text = await files[0].text()
-    console.log(text)
-  }
-
   return (
     <div className="flex h-dvh items-center justify-center gap-10 text-4xl">
-      {/* <div className="aspect-video w-full max-w-2xl">
-        <DragAndDrop onFilesLoad={callback} accept="text/plain" />
-      </div> */}
       <LanguageSelector />
-      {/* <Experience /> */}
       <span className="w-20">{t('mainPage.title')}</span>
     </div>
   )
