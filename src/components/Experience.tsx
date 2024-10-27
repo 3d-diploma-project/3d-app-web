7d0b98665c3a223e00d623d81e139684bcb35d33
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2024-10-27 18:12:43 +0000

    Removed unused button

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,16 +1,15 @@
 import Scene from '@/components/Scene'
-import { useDebugUI } from '@/hooks/useDebugUI'
+
 import { Canvas } from '@react-three/fiber'
 
 const Experience = () => {
-  const { position } = useDebugUI()
   return (
     <Canvas
       camera={{
         fov: 45,
         near: 0.001,
         far: 10000,
-        position
+        position: [3, 3, 3]
       }}
     >
       <Scene />
