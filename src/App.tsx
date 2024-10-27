2533adafd87fb2f037855aefd70e51f726fb3b74
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2024-10-27 17:59:27 +0000

    Fixed model page on small screens

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ModelViewPage from './pages/ModelViewPage'
 const App = () => {
   return (
     <Router>
-      <div className="grid h-dvh grid-rows-[auto,1fr] text-coal-black">
+      <div className="grid h-dvh grid-rows-1 md:grid-rows-[auto,1fr] text-coal-black">
         <Navbar />
         <Routes>
           <Route path="/" index element={<MainPage />} />
