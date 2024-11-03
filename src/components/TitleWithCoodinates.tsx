0f95411f2a31dd380a9dc2f6a5d0ed56cd5955dc
ut5tw <sheredan2003@gmail.com>
2024-11-03 15:48:41 +0200

    Align coordinates with figma design

--- src/components/TitleWithCoodinates.tsx
+++ src/components/TitleWithCoodinates.tsx
@@ -7,18 +7,18 @@ interface TitleWithCoodinatesProps {
 
 const TitleWithCoodinates = ({ title, x = 0, y = 0, z = 0 }: TitleWithCoodinatesProps) => {
   return (
-    <div className="flex justify-between gap-2 font-semibold">
+    <div className="flex flex-col gap-1 font-semibold">
       <p>{title}</p>
-      <div className="flex items-center gap-1">
-        <p className="flex gap-1 rounded-full bg-[#EAF4FF] px-2">
+      <div className="flex items-center gap-2">
+        <p className="flex flex-1 justify-center gap-1 rounded-full bg-[#EAF4FF] px-2">
           <span className="text-app-blue">X</span>
           <span>{x}</span>
         </p>
-        <p className="flex gap-1 rounded-full bg-[#EAF4FF] px-2">
+        <p className="flex flex-1 justify-center gap-1 rounded-full bg-[#EAF4FF] px-2">
           <span className="text-app-blue">Y</span>
           <span>{y}</span>
         </p>
-        <p className="flex gap-1 rounded-full bg-[#EAF4FF] px-2">
+        <p className="flex flex-1 justify-center gap-1 rounded-full bg-[#EAF4FF] px-2">
           <span className="text-app-blue">Z</span>
           <span>{z}</span>
         </p>
