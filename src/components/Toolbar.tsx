e1d08d09824d4611ea3ea13065ec291b108e8d77
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2024-12-19 00:13:05 +0000

    Added translation for languages

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -4,33 +4,49 @@ import SwitchWithTitle from '@/components/SwitchWithTitle'
 import TitleWithCoodinates from '@/components/TitleWithCoodinates'
 import { useAppDispatch, useAppSelector } from '@/hooks/use-redux.ts'
 import { setDisplayNodeIndices } from '@/redux/slices/modelSlice.ts'
+import { useTranslation } from 'react-i18next'
 
 const Toolbar = () => {
   const dispatch = useAppDispatch()
   const { displayNodeIndices } = useAppSelector((store) => store.model)
 
+  const { t } = useTranslation()
+
   return (
     <div className="absolute right-14 z-10 flex max-h-full select-none flex-col gap-3 overflow-y-auto rounded-3xl p-3 py-10 shadow-md backdrop-blur-sm">
-      <TitleWithCoodinates title="Координати" />
-      <TitleWithCoodinates title="Обертання" />
+      <TitleWithCoodinates title={t('toolbar.toolbarSections.coordinates')} />
+      <TitleWithCoodinates title={t('toolbar.toolbarSections.rotation')} />
 
-      <ColorPicker title="Колір фону" />
-      <ColorPicker title="Колір моделі" />
-      <ColorPicker title="Колір вузлів" />
+      <ColorPicker title={t('toolbar.toolbarSections.background')} />
+      <ColorPicker title={t('toolbar.toolbarSections.modelColor')} />
+      <ColorPicker title={t('toolbar.toolbarSections.nodesColor')} />
 
-      <SwitchWithTitle label="Вузли" id="faces" />
+      <SwitchWithTitle label={t('toolbar.toolbarSections.switchSection.nodes')} id="faces" />
       <SwitchWithTitle
-        label="Номери вузлів"
+        label={t('toolbar.toolbarSections.switchSection.nodeNumbers')}
         id="face-numbers"
         onClick={() => dispatch(setDisplayNodeIndices(!displayNodeIndices))}
       />
-      <SwitchWithTitle label="Легенда" id="legend" />
+      <SwitchWithTitle label={t('toolbar.toolbarSections.switchSection.legend')} id="legend" />
 
-      <ButtonWithTitle title="Напруження у вузлах" buttonText="Завантажити файл" />
-      <ButtonWithTitle title="Переміщення у вузлах" buttonText="Завантажити файл" />
-      <ButtonWithTitle variant="ghost" title="Прикладені сили" buttonText="file.txt" />
-      <ButtonWithTitle buttonText="Завантажити файл" />
-      <ButtonWithTitle title="Закріплені вузли" buttonText="Завантажити файл" />
+      <ButtonWithTitle
+        title={t('toolbar.toolbarSections.buttonsSection.nodeStress')}
+        buttonText={t('toolbar.toolbarSections.buttonsSection.fileUpload')}
+      />
+      <ButtonWithTitle
+        title={t('toolbar.toolbarSections.buttonsSection.nodeDisplacement')}
+        buttonText={t('toolbar.toolbarSections.buttonsSection.fileUpload')}
+      />
+      <ButtonWithTitle
+        variant="ghost"
+        title={t('toolbar.toolbarSections.buttonsSection.nodeForces')}
+        buttonText="file.txt"
+      />
+      <ButtonWithTitle buttonText={t('toolbar.toolbarSections.buttonsSection.fileUpload')} />
+      <ButtonWithTitle
+        title={t('toolbar.toolbarSections.buttonsSection.nodeFixes')}
+        buttonText={t('toolbar.toolbarSections.buttonsSection.fileUpload')}
+      />
     </div>
   )
 }
--- src/translations/ua/index.ts
+++ src/translations/ua/index.ts
@@ -1,12 +1,14 @@
 import filesUploader from './files-uploader.json'
 import mainPage from './main-page.json'
 import navbar from './navbar.json'
+import toolbar from './toolbar.json'
 
 const ua = {
   translation: {
     mainPage,
     navbar,
-    filesUploader
+    filesUploader,
+    toolbar
   }
 }
 
