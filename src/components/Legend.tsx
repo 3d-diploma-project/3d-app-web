953d2e04b9e09f9d129da53bb4baa03148a8543d
mborodin <m.borodin.1999@gmail.com>
2025-02-20 09:56:50 +0100

    TET-168 tried to resolve sonar error

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -2,7 +2,11 @@ import LegendItem from '@/components/LegendItem'
 import { useAppSelector } from '@/hooks/use-redux'
 
 const Legend = () => {
-  const legendColors = useAppSelector((store) => store.legend.legend)
+  const legendColors = useAppSelector((store) => store.legend?.legend)
+
+  if (!legendColors) {
+    return
+  }
 
   return (
     <div className="absolute z-10 ml-44 flex flex-col justify-center bg-red-500">
